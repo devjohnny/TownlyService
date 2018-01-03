fd734f1437544651a348d2fcc06dfcfb6f290e82
andras.jani <andras.jani@dealogic.com>
2018-01-03 19:39:53 +0100

    Add jsonformatter and set the defaults

--- TownlyService/TownlyService/App_Start/WebApiConfig.cs
+++ TownlyService/TownlyService/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,7 +14,9 @@ namespace TownlyService
             // Web API configuration and services
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
-			
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
